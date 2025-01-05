5485fa156d5cafc6cc1d1c72cf14c9b24c5f5a6c
Sayan Saha <sayan199783@gmail.com>
2025-01-05 16:00:21 +0530

    First Route

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './Header';
+import {BrowserRouter, Route, Router, Routes} from "react-router-dom"
+import Home from './Home';
+import About from './About';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
     <Header/>
+    <BrowserRouter>
+       <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/about' element={<About/>}></Route>
+       </Routes>
+    </BrowserRouter>
   </div>
 
 );
